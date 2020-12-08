3f6a53b40fbbda175c8d80416713f444abc48c07
Himanshuma38966 <74598469+Himanshuma38966@users.noreply.github.com>
2020-12-08 19:20:04 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    
+    constructor(x,y,radius,color) {
+        var options = { isStatic:false, 
+            restitution:0.3, 
+            friction:0.5,
+             density:1.2 }
+
+        this.body = Bodies.circle(x,y,radius,options);
+        this.radius= radius;
+        this.color = color;
+        this.image = loadImage("paper.png");
+        World.add(world,this.body);
+    }
+
+    display() {
+       
+        ellipseMode(RADIUS);
+        
+        fill(this.color);
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+        push();
+    
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop()
+    }
+}
